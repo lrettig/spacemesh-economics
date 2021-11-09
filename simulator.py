5c20a0585c42f14af3a261fa950793aba3890a90
Lane Rettig <lanerettig@gmail.com>
2021-11-09 13:25:41 -0500

    Add total issuance, decimals

--- simulator.py
+++ simulator.py
@@ -1,5 +1,12 @@
 import datetime, math
 
+# Issuance cap
+# 2.4 bn + 18 decimal places, i.e., 2.4e27
+TOTAL = 2_400_000_000_000_000_000_000_000_000
+
+# Decimal places to remove when printing (i.e., print thousands)
+FMT_DECIMALS = 21
+
 # Genesis timestamp
 GENESIS = datetime.datetime(2022, 1, 1)
 
@@ -11,8 +18,16 @@ PERIODS = math.floor(351.868 * 30 * 24 * 12)
 LAMBDA = math.log(2)/PERIODS
 
 print(f'Running for {PERIODS} periods from {GENESIS} with lambda {LAMBDA} per period')
+print('Following figures in thousands of Smesh:')
 
 curtime = GENESIS
+tot = 0
 for i in range(PERIODS):
-    print(f'Period {i} datetime {curtime}')
     curtime += datetime.timedelta(minutes=5)
+    # Calculate total issuance this period
+    tot_i = TOTAL * (1 - math.exp(-LAMBDA*(i+1)))
+    new = tot_i - tot
+    tot = tot_i
+
+    print(f'Period {i:10} (end {curtime}): {new/10**FMT_DECIMALS:10.3f} new '
+          f'{tot/10**FMT_DECIMALS:10.3f} tot')
